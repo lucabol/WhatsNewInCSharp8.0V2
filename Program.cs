7b22790e55a1c03d680f97d02c4e096645425411
Luca Bolognese <lucabol@microsoft.com>
2019-09-26 13:52:06 +0200

    First Chekin.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,27 @@
 ﻿using System;
+using static System.Console;
 
+class Quote
+{
+    public string Ticker { get; set; }
+    public double Price { get; set; }
+    public string Exchange { get; set; }
 
+    public Quote(string ticker, double price) => (Ticker, Price, Exchange) = (ticker, price, null);
+
+    public static string GetExchangeCountry(Quote q)
+    {
+        string exchange = q.Exchange;
+        return exchange.StartsWith("BVB") ? "Romania" : "TheRest";
+    }
+}
+
+static class Program
+{
+    static void Main()
+    {
+        var q = new Quote("TLV", 2.41);
+        var country = Quote.GetExchangeCountry(q);
+        WriteLine(country);
+    }
+}
