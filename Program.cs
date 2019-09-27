365b28b661a9fdebfce0f1b65238f6a38c2de9e2
Luca Bolognese <lucabol@microsoft.com>
2019-09-27 10:10:24 +0200

    First commit

--- Program.cs
+++ Program.cs
@@ -1,39 +1,25 @@
-﻿using static System.Console;
-
-public readonly struct Bond
-{
-    public readonly double FaceValue;
-    public readonly double Interest;
-    public Bond(double f, double i) => (FaceValue, Interest) = (f, i);
-}
-
-interface IPricer
-{
-    double Price(Bond b);
-}
-
-class StandardPricer : IPricer
-{
-    double IPricer.Price(Bond b) => b.FaceValue * b.Interest / 0.1;
-}
-
-class OptimizedPricer : IPricer
-{
-    double IPricer.Price(Bond b) => (b.FaceValue * b.Interest / 0.1) * 2;
-}
-
-// ... Several other Pricers ...
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
 
 public class Program
 {
+    static Random rnd = new Random();
+
+    static List<double> GetPrices() => rnd.NextDouble() > 0.5 ? null : new List<double>();
+
     static void Main()
     {
-        var b = new Bond(1_000, 0.05);
 
-        IPricer st = new StandardPricer();
-        WriteLine(st.Price(b));
+        var prices = GetPrices();
+        prices ??= new List<double>();
 
-        st = new OptimizedPricer();
-        WriteLine(st.Price(b));
+        int? i = null;
+        prices.Add(i ??= 17);
+        prices.Add(i ??= 20);
+
+        WriteLine(string.Join(' ', prices));
+        WriteLine(i);
     }
 }
+// Equivalence
