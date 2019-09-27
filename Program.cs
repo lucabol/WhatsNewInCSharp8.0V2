591e025d57f3d2e6e653b5079132bac9d784b3cb
Luca Bolognese <lucabol@microsoft.com>
2019-09-27 10:24:01 +0200

    First checkin

--- Program.cs
+++ Program.cs
@@ -1,25 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using static System.Console;
+﻿using static System.Console;
 
 public class Program
 {
-    static Random rnd = new Random();
-
-    static List<double> GetPrices() => rnd.NextDouble() > 0.5 ? null : new List<double>();
-
     static void Main()
     {
-
-        var prices = GetPrices();
-        prices ??= new List<double>();
-
-        int? i = null;
-        prices.Add(i ??= 17);
-        prices.Add(i ??= 20);
-
-        WriteLine(string.Join(' ', prices));
-        WriteLine(i);
+        var fileName = "IDontExist.txt";
+        var filePath = $@"c:\Really\Don't Look Here\{fileName}";
+        WriteLine(filePath);
     }
 }
-// Equivalence
